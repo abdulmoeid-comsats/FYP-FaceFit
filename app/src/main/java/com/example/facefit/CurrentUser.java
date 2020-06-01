f87af4459657f8c8d4d6cd127148428412d3ec1e
fayaziii332 <syedfayyazshah619.com>
2020-06-01 16:42:28 +0500

    Some Fixes

--- app/src/main/java/com/example/facefit/CurrentUser.java
+++ app/src/main/java/com/example/facefit/CurrentUser.java
@@ -6,7 +6,7 @@ public class CurrentUser {
     public static String frameSelected;
     public static String frameSelectedCategory;
     public static String[] frames = {"done.sfb", "done2.sfb", "done3.sfb", "done4.sfb", "done5.sfb", "done6.sfb", "done7.sfb", "done8.sfb", "done9.sfb", "done10.sfb"
-                                    ,"done11.sfb", "done12.sfb", "done13.sfb", "done14.sfb", "done15.sfb", "done16.sfb", "done17.sfb", "done18.sfb", "done19.sfb", "done20.sfb"
+                                    ,"done11.sfb", "dg.sfb", "done13.sfb", "done14.sfb", "done15.sfb", "done16.sfb", "done17.sfb", "done18.sfb", "done19.sfb", "done20.sfb"
                                         ,"done21.sfb", "done22.sfb", "done23.sfb", "done24.sfb", "done25.sfb", "done26.sfb", "done27.sfb", "done28.sfb", "done29.sfb", "done30.sfb"};
 
 
